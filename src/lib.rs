b97055dc2bb8a91cc74b88ae9e1330e734f3d996
Jan Likar <likar.jan@gmail.com>
2016-04-12 16:18:10 +0200

    Itertools removal and work on #1

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@
 
 extern crate cargo;
 extern crate walkdir;
-extern crate itertools;
 
 macro_rules! bail {
     ($($fmt:tt)*) => (
@@ -17,7 +16,7 @@ macro_rules! bail {
 }
 
 pub mod ops {
-    use std::path::{Path, PathBuf};
+    use std::path::Path;
     use std::fs;
     use std::env;
 
@@ -30,8 +29,6 @@ pub mod ops {
     use cargo::sources::RegistrySource;
 
     use walkdir::WalkDir;
-    use itertools::Itertools;
-    use itertools::EitherOrBoth::{Both, Left, Right};
 
     pub fn clone(krate: &Option<String>,
                  srcid: &SourceId,
@@ -44,8 +41,19 @@ pub mod ops {
                 None => bail!("specify which package to clone!"),
         };
 
-        let mut regsrc = RegistrySource::new(&srcid, &config);
-        try!(regsrc.update());
+        let mut src = if srcid.is_registry() {
+            RegistrySource::new(&srcid, &config)
+        }
+        else if srcid.is_path(){
+            //PathSource::new(PATH , &srcid, &config)
+            unimplemented!();
+        }
+        else {
+            //GitSource::new(&srcid, &config)
+            unimplemented!();
+        };
+
+        try!(src.update());
 
         let version = match flag_version {
             Some(v) => {
@@ -56,7 +64,7 @@ pub mod ops {
             },
             None => {
                 let dep = try!(Dependency::parse(krate, flag_version.as_ref().map(|s| &s[..]), &srcid));
-                let summaries = try!(regsrc.query(&dep));
+                let summaries = try!(src.query(&dep));
 
                 let latest = summaries.iter().max_by_key(|s| s.version());
 
@@ -69,7 +77,7 @@ pub mod ops {
 
         let pkgid = try!(PackageId::new(&krate, version, srcid));
 
-        let krate = try!(regsrc.download(&pkgid.clone()));
+        let krate = try!(src.download(&pkgid.clone()));
 
         let mut dest_path = try!(env::current_dir());
         dest_path.push(krate.name());
@@ -84,7 +92,7 @@ pub mod ops {
             let entry = entry.unwrap();
             let file_type = entry.file_type();
             let mut to = to.to_owned();
-            to.push(&strip_prefix(from, entry.path()));
+            to.push(entry.path().strip_prefix(from).unwrap());
 
             if file_type.is_file() {
                 try!(fs::copy(&entry.path(), &to));
@@ -95,28 +103,5 @@ pub mod ops {
         }
 
         Ok(())
-    }
-
-    // When Path.strip_prefix() lands, this can be removed
-    fn strip_prefix(prefix: &Path, path: &Path) -> PathBuf {
-        assert!(path.starts_with(prefix));
-        let mut ret = PathBuf::new();
-        for e in path.iter().zip_longest(prefix) {
-            match e {
-                Both(..) => continue,
-                Left(a) => ret.push(a),
-                Right(_) => unreachable!(),
-            }
-        }
-
-        ret
-    }
-
-    #[test]
-    fn test_strip_prefix() {
-        let r = Path::new("foo/bar.py");
-        let prefix = Path::new("/home/john/");
-        let path = Path::new("/home/john/foo/bar.py");
-        assert_eq!(r, strip_prefix(prefix, path));
     }
 }
