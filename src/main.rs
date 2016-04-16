5a6e565d1b67de2ebcb895327f6b0a859f5e4b91
Jan Likar <likar.jan@gmail.com>
2016-04-16 20:58:33 +0200

    Cleanup before publishing

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ extern crate cargo_clone;
 
 use std::io::{self, Write};
 
-use cargo::core::{SourceId, GitReference};
-use cargo::util::{Config, CliResult, ToUrl, human};
+use cargo::core::SourceId;
+use cargo::util::{Config, CliResult};
 
 use docopt::Docopt;
 
@@ -29,13 +29,6 @@ pub struct Options {
 
     arg_crate: Option<String>,
     flag_vers: Option<String>,
-
-    flag_git: Option<String>,
-    flag_branch: Option<String>,
-    flag_tag: Option<String>,
-    flag_rev: Option<String>,
-
-    flag_path: Option<String>,
 }
 
 pub const USAGE: &'static str = "
@@ -48,13 +41,6 @@ Options:
     --prefix DIR              Directory to clone the package into
 
     --vers VERS               Specify a version to clone from crates.io
-
-    --git URL                 Git URL to clone the specified crate from
-    --branch BRANCH           Branch to use when cloning from git
-    --tag TAG                 Tag to use when cloning from git
-    --rev SHA                 Specific commit to use when cloning from git
-
-    --path PATH               Filesystem path to local crate to clone
 
     -h, --help                Print this message
     -V, --version             Print version information
@@ -89,32 +75,8 @@ pub fn execute(options: Options, config: Config) -> CliResult<Option<()>> {
                                 options.flag_quiet,
                                 &options.flag_color));
 
-    let source_id = match (options.flag_path, options.flag_git) {
-        (Some(path), None) => {
-            try!(SourceId::for_path(path.as_ref()))
-        }
-        (None, Some(git)) => {
-            unimplemented!();
-            // SourceId for Git registry
-            let git = try!(git.to_url().map_err(human));
-            let gref = if let Some(branch) = options.flag_branch {
-                GitReference::Branch(branch)
-            } else if let Some(tag) = options.flag_tag {
-                GitReference::Tag(tag)
-            } else if let Some(rev) = options.flag_rev {
-                GitReference::Rev(rev)
-            } else {
-                GitReference::Branch("master".to_string())
-            };
-
-            SourceId::for_git(&git, gref)
-        },
-        (None, None) => {
-            // Make a SourceId for the central Registry (usually crates.io)
-            try!(SourceId::for_central(&config))
-        }
-        (Some(_), Some(_)) => panic!("--path and --git flags are incompatible."),
-    };
+    // Make a SourceId for the central Registry (usually crates.io)
+    let source_id = try!(SourceId::for_central(&config));
 
     let krate = options.arg_crate.as_ref().map(|s| &s[..]);
     let prefix = options.flag_prefix.as_ref().map(|s| &s[..]);
