27dd28ba88c9cd190fd59f5ef7fa06a5ba5137dc
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2022-12-08 17:30:51 +0100

    source

--- cargo-clone-core/src/cloner.rs
+++ cargo-clone-core/src/cloner.rs
@@ -1,7 +1,7 @@
 use std::{env, path::PathBuf};
 
 use anyhow::Context;
-use cargo::{CargoResult, Config};
+use cargo::{core::SourceId, util::IntoUrl, CargoResult, Config};
 
 pub struct Cloner {
     /// Cargo configuration.
@@ -9,12 +9,53 @@ pub struct Cloner {
     /// Directory where the crates will be cloned.
     /// Each crate is cloned into a subdirectory of this directory.
     directory: PathBuf,
+    /// Where the crates will be cloned from.
+    source_id: SourceId,
 }
 
+/// Builder for [`Cloner`].
+/// By default the cloner will clone from crates.io.
 #[derive(Debug, Default)]
 pub struct ClonerBuilder {
     config: Option<Config>,
     directory: Option<PathBuf>,
+    source: Source,
+}
+
+#[derive(Debug, Default)]
+pub enum Source {
+    #[default]
+    CratesIo,
+    ///
+    Index(String),
+    LocalRegistry(String),
+    Registry(String),
+}
+
+impl Source {
+    /// Creates a [`Source`] from the name of the remote registry.
+    pub fn registry(self, key: impl Into<String>) -> Self {
+        Source::Registry(key.into())
+    }
+
+    /// Creates a [`Source`] from a local registry path.
+    pub fn local_registry(self, path: impl Into<String>) -> Self {
+        Source::LocalRegistry(path.into())
+    }
+
+    /// Creates a [`Source`] from a remote registry URL.
+    pub fn index(self, index: impl Into<String>) -> Self {
+        Source::Index(index.into())
+    }
+
+    fn to_source_id(&self, config: &Config) -> CargoResult<SourceId> {
+        match self {
+            Source::CratesIo => SourceId::crates_io(config),
+            Source::Index(url) => SourceId::for_registry(&url.into_url()?),
+            Source::LocalRegistry(path) => SourceId::for_local_registry(&config.cwd().join(path)),
+            Source::Registry(key) => SourceId::alt_registry(config, key),
+        }
+    }
 }
 
 impl ClonerBuilder {
@@ -29,11 +70,16 @@ impl ClonerBuilder {
         }
     }
 
-    pub fn with_directory(self, directory: PathBuf) -> Self {
+    pub fn with_directory(self, directory: impl Into<PathBuf>) -> Self {
         Self {
-            directory: Some(directory),
+            directory: Some(directory.into()),
             ..self
         }
+    }
+
+    /// Clone from an alternative source, instead of crates.io.
+    pub fn with_source(self, source: Source) -> Self {
+        Self { source, ..self }
     }
 
     pub fn build(self) -> CargoResult<Cloner> {
@@ -46,6 +92,14 @@ impl ClonerBuilder {
             None => env::current_dir().context("Unable to get current directory.")?,
         };
 
-        Ok(Cloner { config, directory })
+        let source_id = self
+            .source
+            .to_source_id(&config)
+            .context("can't determine the source id")?;
+        Ok(Cloner {
+            config,
+            directory,
+            source_id,
+        })
     }
 }
